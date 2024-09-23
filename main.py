0825a6f1dd95f92e4c4c097408e2fa13391f9e51
stmata <108939392+stmata@users.noreply.github.com>
2024-09-23 15:07:32 -0400

    FirstApp
    Our First App to Azure

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-mylist = [x for x in range(10)]
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    try:
+        return {"message": "hello from server"}
+    except Exception as e:
+        print(f'Exception: {e}')
+        raise HTTPException(status_code=500, detail=str(e))
+@app.get("/test")
+async def hello():
+    try:
+        return {"message": "hello from test server"}
+    except Exception as e:
+        print(f'Exception: {e}')
+        raise HTTPException(status_code=500, detail=str(e))
